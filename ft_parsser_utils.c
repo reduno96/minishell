596494823715d0d7d3d2e4a84313e9d74ac56bec
reduno96 <reduno96@gmail.com>
2024-08-16 21:45:53 +0000

    Refactor ft_parsser_utils.c for improved parsing utilities

--- ft_parsser_utils.c
+++ ft_parsser_utils.c
@@ -6,7 +6,7 @@
 /*   By: rel-mora <reduno96@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 21:25:06 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/08/12 21:58:32 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/08/16 21:44:28 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,32 @@ t_command	*ft_last_command(t_command *lst)
 	return (last);
 }
 
+void	ft_her_doc(t_command **new_node)
+{
+	t_redirect	*tmp;
+	int			i;
+
+	i = 0;
+	tmp = (*new_node)->doc;
+	while (tmp != NULL)
+	{
+		if (tmp->type == HERE_DOC)
+			(*new_node)->len++;
+		tmp = tmp->next;
+	}
+	(*new_node)->store_her = malloc(sizeof(char *) * (*new_node)->len);
+	tmp = (*new_node)->doc;
+	while (tmp != NULL)
+	{
+		if (tmp->type == HERE_DOC)
+		{
+			(*new_node)->store_her[i] = ft_strdup(tmp->store);
+			i++;
+		}
+		tmp = tmp->next;
+	}
+	(*new_node)->store_her[i] = NULL;
+}
 t_command	*ft_new_command(int count, t_splitor **tmp_x)
 {
 	t_command	*new_node;
@@ -47,6 +73,7 @@ t_command	*ft_new_command(int count, t_splitor **tmp_x)
 	i = 0;
 	new_node = malloc(sizeof(t_command));
 	new_node->arg = malloc(sizeof(char *) * (count + 1));
+	new_node->len = 0;
 	if (((*tmp_x) != NULL && (*tmp_x)->type == '|'))
 	{
 		new_node->arg[i] = ft_strdup((*tmp_x)->in);
@@ -60,5 +87,6 @@ t_command	*ft_new_command(int count, t_splitor **tmp_x)
 	new_node->content = new_node->arg[0];
 	new_node->doc = NULL;
 	ft_check_doc(&new_node);
+	ft_her_doc(&new_node);
 	return (new_node);
 }
