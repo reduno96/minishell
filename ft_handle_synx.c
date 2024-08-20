a32ceb1a49a65d9f5c9a0ab6e60b01a38ae139f0
reduno96 <reduno96@gmail.com>
2024-08-20 19:49:18 +0000

    Refactor ft_handle_synx.c for improved code readability and maintainability

--- ft_handle_synx.c
+++ ft_handle_synx.c
@@ -6,7 +6,7 @@
 /*   By: rel-mora <reduno96@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 07:47:51 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/08/19 01:01:51 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/08/20 19:48:37 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int	ft_condition(t_splitor *start)
 {
 	if ((start->type != ' ' && start->type != -1 && start->type != '$'
-			&& start->type != '\'' && start->type != '\"'))
+			&& start->type != '\'' && start->type != '\"'
+			&& start->type != HERE_DOC))
 		return (1);
 	return (0);
 }
@@ -24,14 +25,16 @@ int	ft_check_between(t_splitor **start)
 {
 	while ((*start) != NULL)
 	{
-		if ((*start)->type == '|' || (ft_condition(*start)
-				&& ((*start)->state == G)))
+		if ((*start)->type == '|' || (*start)->type == HERE_DOC
+			|| (ft_condition(*start) && ((*start)->state == G)))
 		{
 			if ((*start) != NULL)
 				(*start) = (*start)->next;
 			ft_skip_spaces(&(*start));
 			if ((*start) == NULL || (ft_condition(*start)))
+			{
 				return (1);
+			}
 		}
 		else if (ft_condition(*start) && (*start)->state != G)
 			while (((*start) != NULL) && (ft_condition(*start)
