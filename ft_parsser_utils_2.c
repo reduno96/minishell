395676db8bc015f544bc7ba4a596f980528b7f58
reduno96 <reduno96@gmail.com>
2024-08-22 14:21:02 +0100

    modification

--- ft_parsser_utils_2.c
+++ ft_parsser_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 18:00:12 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/08/22 12:00:09 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/08/22 14:18:32 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,12 @@ void	ft_double_and_sigle(t_splitor **tmp_x, int *i, t_command **new_node)
 	(*new_node)->arg[*i] = NULL;
 	(*new_node)->next = NULL;
 }
-void	ft_general_cmd(t_command **new_node, int *i, t_splitor **tmp_x)
+
+void	ft_general_command(t_command **new_node, int *i, t_splitor **tmp_x)
 {
-	if ((*tmp_x) != NULL && (*tmp_x)->state == G && (*tmp_x)->type == -1)
+	if ((*tmp_x) != NULL && (*tmp_x)->state == G && (*tmp_x)->type == -1
+		&& (*tmp_x)->next != NULL && ((*tmp_x)->next->type == '\"'
+			|| (*tmp_x)->next->type == '\''))
 	{
 		(*new_node)->arg[*i] = ft_strdup("");
 		(*new_node)->arg[*i] = ft_strjoin((*new_node)->arg[*i], (*tmp_x)->in);
@@ -57,24 +60,22 @@ void	ft_general_cmd(t_command **new_node, int *i, t_splitor **tmp_x)
 					(*tmp_x)->in);
 			(*tmp_x) = (*tmp_x)->next;
 		}
-		(*i)++;
-		(*new_node)->arg[*i] = NULL;
-		(*new_node)->next = NULL;
 	}
 	else
 	{
+		(*new_node)->arg[*i] = ft_strdup("");
 		(*new_node)->arg[*i] = ft_strdup((*tmp_x)->in);
-		(*i)++;
-		(*new_node)->arg[*i] = NULL;
-		(*new_node)->next = NULL;
 		(*tmp_x) = (*tmp_x)->next;
 	}
+	(*i)++;
+	(*new_node)->arg[*i] = NULL;
+	(*new_node)->next = NULL;
 }
 void	ft_neuter_cmd(t_command **new_node, int *i, t_splitor **tmp_x)
 {
 	if ((*tmp_x) != NULL && (*tmp_x)->state == G && (*tmp_x)->type != '\"'
 		&& (*tmp_x)->type != '\'' && (*tmp_x)->type != '|')
-		ft_general_cmd(new_node, i, tmp_x);
+		ft_general_command(new_node, i, tmp_x);
 	else if ((*tmp_x) != NULL && ((*tmp_x)->state == D || (*tmp_x)->state == S))
 		ft_double_and_sigle(tmp_x, i, new_node);
 	else if (((*tmp_x) != NULL && (*tmp_x)->state == G)
