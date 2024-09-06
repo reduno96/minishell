8283e94928b726b04eb71b07b2d7829948c7def8
reduno96 <reduno96@gmail.com>
2024-09-06 15:38:58 +0100

    Fix syntax and formatting issues in ft_handle_synx.c

--- ft_handle_synx.c
+++ ft_handle_synx.c
@@ -6,7 +6,7 @@
 /*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 07:47:51 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/09/02 19:08:25 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/09/06 11:51:35 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,9 @@ int	ft_check_between(t_splitor **start)
 			if ((*start) != NULL)
 				(*start) = (*start)->next;
 			ft_skip_spaces(&(*start));
-			if ((*start) == NULL ||  ((*start)->type == '|' && (ft_condition(*start)) && (*start)->state == G))
-			{
-				// printf("Hi i'm in ft_check_between\n");
+			if ((*start) == NULL || ((*start)->type == '|'
+					&& (ft_condition(*start)) && (*start)->state == G))
 				return (1);
-			}
 		}
 		else if (ft_condition(*start) && (*start)->state != G)
 			while (((*start) != NULL) && (ft_condition(*start)
@@ -62,16 +60,10 @@ int	ft_handler_syn_error(t_splitor **x)
 	if (start->type == '|' || ((start->type != ' ' && start->type != -1
 				&& start->type != '$') && start->next == NULL)
 		|| ((start->type == '\'' || start->type == '\"')
-				&& start->next == NULL))
-	{
-		// printf("Hi i'm in first condion \n");
+			&& start->next == NULL))
 		return (1);
-	}
 	if (ft_check_between(&start))
-	{
-		// printf("Hi i'm in second condion \n");
 		return (1);
-	}
 	start = *x;
 	end = ft_lstlast(start);
 	if (end->type == '|')
