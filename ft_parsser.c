6d7f37f935a6fb4bef139f1faace7c265d98058d
reduno96 <reduno96@gmail.com>
2024-09-06 15:39:45 +0100

    Update ft_command function

--- ft_parsser.c
+++ ft_parsser.c
@@ -6,7 +6,7 @@
 /*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 18:00:47 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/09/04 12:51:20 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/09/06 08:24:00 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -122,8 +122,8 @@ void	ft_command(t_splitor **x, t_command **cmd, t_envarment *my_env)
 {
 	int			count;
 	t_splitor	*tmp_x;
-	// t_command	*tmp_cmd;
-	// int			i;
+	t_command	*tmp_cmd;
+	int			i;
 
 	// cmd = NULL;
 	tmp_x = *x;
@@ -138,39 +138,39 @@ void	ft_command(t_splitor **x, t_command **cmd, t_envarment *my_env)
 	}
 	ft_fill_red(cmd, x, my_env);
 	ft_fill_her(cmd);
-	// i = 0;
-	// tmp_cmd = *cmd;
-	// while (tmp_cmd != NULL)
-	// {
-	// 	printf("\033[0;32m\n\t++++++++++++++   Command   ++++++++++++++++\n\033[0m");
-	// 	if (tmp_cmd->content != NULL)
-	// 	{
-	// 		printf("Content :		%s \n", tmp_cmd->content);
-	// 	}
-	// 	if (tmp_cmd->arg != NULL && tmp_cmd->arg[0] != NULL)
-	// 	{
-	// 		printf("Argument :	");
-	// 		i = 0; // Initialize i before using it
-	// 		while (tmp_cmd->arg[i] != NULL)
-	// 		{
-	// 			printf(" [%s] ", tmp_cmd->arg[i]);
-	// 			i++;
-	// 		}
-	// 	}
-	// 	printf("\n");
-	// 	printf("doc :		\n");
-	// 	print_redirect_list(tmp_cmd->doc);
-	// 	// printf("\n");
-	// 	i = 0;
-	// 	printf("HerDoc :		\n");
-	// 	if (tmp_cmd->store_her != NULL && tmp_cmd->store_her[0] != NULL)
-	// 		while (tmp_cmd->store_her[i] != NULL)
-	// 		{
-	// 			if (tmp_cmd->store_her[i] == '\0')
-	// 				printf("____in herdoc print_________\n");
-	// 			printf("HerDoc ==>> %s \n\n", tmp_cmd->store_her[i]);
-	// 			i++;
-	// 		}
-	// 	tmp_cmd = tmp_cmd->next;
-	// }
+	i = 0;
+	tmp_cmd = *cmd;
+	while (tmp_cmd != NULL)
+	{
+		printf("\033[0;32m\n\t++++++++++++++   Command   ++++++++++++++++\n\033[0m");
+		if (tmp_cmd->content != NULL)
+		{
+			printf("Content :		%s \n", tmp_cmd->content);
+		}
+		if (tmp_cmd->arg != NULL && tmp_cmd->arg[0] != NULL)
+		{
+			printf("Argument :	");
+			i = 0; // Initialize i before using it
+			while (tmp_cmd->arg[i] != NULL)
+			{
+				printf(" [%s] ", tmp_cmd->arg[i]);
+				i++;
+			}
+		}
+		printf("\n");
+		printf("doc :		\n");
+		print_redirect_list(tmp_cmd->doc);
+		// printf("\n");
+		i = 0;
+		printf("HerDoc :		\n");
+		if (tmp_cmd->store_her != NULL && tmp_cmd->store_her[0] != NULL)
+			while (tmp_cmd->store_her[i] != NULL)
+			{
+				if (tmp_cmd->store_her[i] == '\0')
+					printf("____in herdoc print_________\n");
+				printf("HerDoc ==>> %s \n\n", tmp_cmd->store_her[i]);
+				i++;
+			}
+		tmp_cmd = tmp_cmd->next;
+	}
 }
