40b3420f90895ed0b272f6baf5c40afb4d030b02
reduno96 <reduno96@gmail.com>
2024-09-08 07:34:18 +0100

    Add lexer utility functions

---
+++ lexer_utils_2.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_utils_2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 16:43:45 by rel-mora          #+#    #+#             */
+/*   Updated: 2024/09/06 16:46:39 by rel-mora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_get_word(char *s, t_idx *var, t_splitor **x)
+{
+	while (s[var->i] && !ft_check_input(s[var->i]))
+	{
+		var->state = ft_get_state(var, s[var->i]);
+		var->i++;
+		var->len++;
+	}
+	ft_add(x, ft_lstnew(ft_substr(s, var->start, var->len), var->len, WORD,
+			var->state));
+}
+
+void	ft_her_dir(t_splitor **x, t_idx *var, char *s)
+{
+	var->len++;
+	var->i++;
+	if (var->len >= 1 && s[var->i] == '<')
+		ft_add(x, ft_lstnew(ft_substr(s, var->start, var->len), var->len,
+				HERE_DOC, ft_get_state(var, s[var->i])));
+	else if (var->len >= 1 && s[var->i] == '>')
+		ft_add(x, ft_lstnew(ft_substr(s, var->start, var->len), var->len,
+				DREDIR_OUT, ft_get_state(var, s[var->i])));
+}
+
+void	ft_else(char *s, t_idx *var)
+{
+	var->state = ft_get_state(var, s[var->i]);
+	var->i++;
+	var->len++;
+}
+
+void	ft_get_env(char *s, t_idx *var, t_splitor **x)
+{
+	if (s[var->i] && s[var->i] == '$' && ft_isalnum(s[var->i + 1])
+		&& !ft_check_input(s[var->i + 1]))
+	{
+		while (s[var->i] && !ft_check_input(s[var->i + 1])
+			&& ft_isalnum(s[var->i + 1]))
+		{
+			var->state = ft_get_state(var, s[var->i]);
+			var->i++;
+			var->len++;
+		}
+	}
+	else if (s[var->i] && s[var->i] == '$' && !ft_isalnum(s[var->i + 1])
+		&& !ft_check_input(s[var->i + 1]))
+	{
+		while (s[var->i] && !ft_isalnum(s[var->i + 1]))
+		{
+			var->state = ft_get_state(var, s[var->i]);
+			var->i++;
+			var->len++;
+		}
+	}
+	else if (s[var->i] && s[var->i] == '$' && s[var->i + 1] == '?')
+		ft_else(s, var);
+	ft_add(x, ft_lstnew(ft_substr(s, var->start, var->len), var->len, ENV,
+			var->state));
+}
+
+void	ft_get_char(char *s, t_idx *var, t_splitor **x)
+{
+	var->len++;
+	if (s[var->i] && s[var->i] == '$')
+		ft_get_env(s, var, x);
+	else
+	{
+		if (s[var->i] && ((s[var->i] == '>' && s[var->i + 1] == '>')
+				|| (s[var->i] == '<' && s[var->i + 1] == '<')))
+			ft_her_dir(x, var, s);
+		else
+			ft_add(x, ft_lstnew(ft_substr(s, var->start, var->len), var->len,
+					ft_get_token(s[var->i]), ft_get_state(var, s[var->i])));
+	}
+	var->i++;
+}
