b9e124aceeddc6ee8c2f1bfc0aac717c757c31ed
reduno96 <reduno96@gmail.com>
2024-09-21 20:38:54 +0100

    modification redirection and skip and join name the file

--- ft_direction.c
+++ ft_direction.c
@@ -6,85 +6,11 @@
 /*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 07:24:52 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/09/20 12:43:06 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/09/21 19:54:32 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	print_redirect_list(t_redirect *head)
-{
-	t_redirect	*current;
-
-	(void)head;
-	current = head;
-	while (current != NULL)
-	{
-		printf("type : %d | %s\n", current->type, current->store);
-		current = current->next;
-	}
-}
-
-void	ft_skip_spaces_and_quotes(t_splitor **tmp_x)
-{
-	while ((*tmp_x) != NULL && ((*tmp_x)->type == ' ' || (*tmp_x)->type == '\"'
-			|| (*tmp_x)->type == '\''))
-		(*tmp_x) = (*tmp_x)->next;
-}
-
-char	*ft_check_ambiguous(char *tmp_x)
-{
-	if (tmp_x == NULL)
-		return (NULL);
-	else if (tmp_x != NULL && !ft_search(tmp_x, " "))
-		return (tmp_x);
-	return (tmp_x);
-}
-
-char	*ft_skip_direction(t_splitor **tmp_x, t_envarment *my_env, int *is_amb,
-		int her)
-{
-	char	*s;
-	char	*final;
-	char	**str;
-
-	str = malloc(sizeof(char *));
-	str[0] = NULL;
-	final = NULL;
-	if (((*tmp_x) != NULL && (*tmp_x)->next != NULL) && (((*tmp_x)->type == '\"'
-				&& (*tmp_x)->next->type == '\"') || ((*tmp_x)->type == '\''
-				&& (*tmp_x)->next->type == '\'')))
-		final = ft_strdup("\0");
-	else if ((*tmp_x) != NULL && (*tmp_x)->state == G && ((*tmp_x)->type == '\"'
-			|| (*tmp_x)->type == '\''))
-	{
-		ft_double_and_sigle(tmp_x, my_env, her, &str);
-		final = str[0];
-	}
-	else if ((*tmp_x) != NULL && (*tmp_x)->state == G)
-	{
-		if ((*tmp_x) != NULL && (*tmp_x)->state == G && (*tmp_x)->type == '$'
-			&& her == 1)
-		{
-			s = ft_expand((*tmp_x)->in, &my_env);
-			if (ft_check_ambiguous(s) == NULL && her == 1)
-				return (*is_amb = 1, NULL);
-			final = ft_strjoin(final, s);
-			return (final);
-		}
-		else if ((*tmp_x) != NULL && (*tmp_x)->state == G)
-		{
-			ft_word(tmp_x, my_env, her, &str);
-			final = str[0];
-		}
-	}
-	return (final);
-}
-void	ft_skip_one_quote(t_splitor **tmp_x)
-{
-	if ((*tmp_x) != NULL && ((*tmp_x)->type == '\'' || (*tmp_x)->type == '\"'))
-		(*tmp_x) = (*tmp_x)->next;
-}
 
 void	ft_next(t_splitor **tmp_x, t_command **tmp_cmd)
 {
@@ -96,43 +22,51 @@ void	ft_next(t_splitor **tmp_x, t_command **tmp_cmd)
 		(*tmp_cmd) = (*tmp_cmd)->next;
 }
 
-int	ft_fill_redirection(t_splitor **tmp_x, t_command *tmp_cmd,
-		t_envarment *my_env)
+void	ft_fill_redirection(t_pre *id, t_envarment *my_env, char *final)
 {
-	char	*final;
-	int		is_amb;
-
-	final = NULL;
-	is_amb = 0;
-	while ((*tmp_x) != NULL && ((*tmp_x)->type != HERE_DOC
-			&& redirection(*tmp_x)))
+	if (id->tmp_x != NULL && id->tmp_x->type == '>' && id->tmp_x->state == G)
 	{
-		if ((*tmp_x) != NULL && (*tmp_x)->type == '>' && (*tmp_x)->state == G)
-		{
-			(*tmp_x) = (*tmp_x)->next;
-			ft_skip_spaces(tmp_x);
-			final = ft_skip_direction(&(*tmp_x), my_env, &is_amb, 0);
-			ft_add_redir((&tmp_cmd->doc), ft_new_redir(final, '>', is_amb));
-		}
-		else if ((*tmp_x) != NULL && (*tmp_x)->type == '<'
-			&& (*tmp_x)->state == G)
-		{
-			(*tmp_x) = (*tmp_x)->next;
-			ft_skip_spaces(tmp_x);
-			final = ft_skip_direction(tmp_x, my_env, &is_amb, 0);
-			ft_add_redir((&tmp_cmd->doc), ft_new_redir(final, '<', is_amb));
-		}
-		else if ((*tmp_x) != NULL && (*tmp_x)->type == DREDIR_OUT
-			&& (*tmp_x)->state == G)
-		{
-			(*tmp_x) = (*tmp_x)->next;
-			ft_skip_spaces(tmp_x);
-			final = ft_skip_direction(tmp_x, my_env, &is_amb, 0);
-			ft_add_redir((&tmp_cmd->doc), ft_new_redir(final, DREDIR_OUT,
-					is_amb));
-		}
+		id->tmp_x = id->tmp_x->next;
+		ft_skip_spaces(&id->tmp_x);
+		final = ft_skip_direction(&id->tmp_x, my_env, &id->is_amb, 1);
+		ft_add_redir(&(id->tmp_cmd->doc), ft_new_redir(final, '>', id->is_amb));
 	}
-	return (0);
+	else if (id->tmp_x != NULL && id->tmp_x->type == '<'
+		&& id->tmp_x->state == G)
+	{
+		id->tmp_x = id->tmp_x->next;
+		ft_skip_spaces(&id->tmp_x);
+		final = ft_skip_direction(&id->tmp_x, my_env, &id->is_amb, 1);
+		ft_add_redir(&(id->tmp_cmd->doc), ft_new_redir(final, '<', id->is_amb));
+	}
+	else if (id->tmp_x != NULL && id->tmp_x->type == DREDIR_OUT
+		&& id->tmp_x->state == G)
+	{
+		id->tmp_x = id->tmp_x->next;
+		ft_skip_spaces(&id->tmp_x);
+		final = ft_skip_direction(&id->tmp_x, my_env, &id->is_amb, 1);
+		ft_add_redir(&(id->tmp_cmd->doc), ft_new_redir(final, DREDIR_OUT,
+				id->is_amb));
+	}
+}
+
+void	ft_fill_her(t_pre *id, t_envarment *my_env, char *final)
+{
+	while ((id->tmp_x != NULL && id->tmp_x->type == HERE_DOC
+			&& id->tmp_x->state == G))
+	{
+		id->tmp_x = id->tmp_x->next;
+		ft_skip_spaces(&id->tmp_x);
+		if (id->tmp_x != NULL && id->tmp_x->state == G
+			&& ((id->tmp_x->next != NULL && id->tmp_x->type == 32)
+				|| id->tmp_x->next == NULL))
+			id->is_expand = 1;
+		final = ft_skip_direction(&id->tmp_x, my_env, 0, 0);
+		ft_add_redir(&(id->tmp_cmd->doc), ft_new_redir(final, HERE_DOC, 0));
+		add_back_node_her(&(id->tmp_cmd->her), new_node_her(final, -1, id->j,
+				0));
+		id->j++;
+	}
 }
 
 void	ft_check_redirection(t_pre *id, t_envarment *my_env)
@@ -140,56 +74,20 @@ void	ft_check_redirection(t_pre *id, t_envarment *my_env)
 	char	*final;
 
 	final = NULL;
-	while ((id->tmp_x) != NULL && redirection(id->tmp_x))
+	while ((id->tmp_x) != NULL && (redirection(id->tmp_x)
+			&& id->tmp_x->state == G))
 	{
-		if (id->tmp_x != NULL && id->tmp_x->type == '>' && id->tmp_x->state == G)
-		{
-			id->tmp_x = id->tmp_x->next;
-			ft_skip_spaces(&id->tmp_x);
-			final = ft_skip_direction(&id->tmp_x, my_env, &id->is_amb, 0);
-			ft_add_redir(&(id->tmp_cmd->doc), ft_new_redir(final, '>', id->is_amb));
-		}
-		else if (id->tmp_x != NULL && id->tmp_x->type == '<'
-			&& id->tmp_x->state == G)
-		{
-			id->tmp_x = id->tmp_x->next;
-			ft_skip_spaces(&id->tmp_x);
-			final = ft_skip_direction(&id->tmp_x, my_env, &id->is_amb, 0);
-			ft_add_redir(&(id->tmp_cmd->doc), ft_new_redir(final, '<', id->is_amb));
-		}
-		else if (id->tmp_x != NULL && id->tmp_x->type == DREDIR_OUT
-			&& id->tmp_x->state == G)
-		{
-			id->tmp_x = id->tmp_x->next;
-			ft_skip_spaces(&id->tmp_x);
-			final = ft_skip_direction(&id->tmp_x, my_env, &id->is_amb, 0);
-			ft_add_redir(&(id->tmp_cmd->doc), ft_new_redir(final, DREDIR_OUT,
-					id->is_amb));
-		}
+		if (id->tmp_x != NULL && (redirection(id->tmp_x)
+				&& id->tmp_x->type != HERE_DOC))
+			ft_fill_redirection(id, my_env, final);
 		else if ((id->tmp_x != NULL && id->tmp_x->type == HERE_DOC
 				&& id->tmp_x->state == G))
-		{
-			while ((id->tmp_x != NULL && id->tmp_x->type == HERE_DOC
-					&& id->tmp_x->state == G))
-			{
-				id->tmp_x = id->tmp_x->next;
-				ft_skip_spaces(&id->tmp_x);
-				if (id->tmp_x != NULL && id->tmp_x->state == G
-					&& ((id->tmp_x->next != NULL && id->tmp_x->type == 32)
-						|| id->tmp_x->next == NULL))
-					id->is_expand = 1;
-				final = ft_skip_direction(&id->tmp_x, my_env, 0, 0);
-				ft_add_redir(&(id->tmp_cmd->doc), ft_new_redir(final, HERE_DOC,
-						0));
-				add_back_node_her(&(id->tmp_cmd->her), new_node_her(final, -1,
-						id->j, 0));
-				id->j++;
-			}
-		}
+			ft_fill_her(id, my_env, final);
 	}
 	if (id->tmp_x != NULL)
 		id->tmp_x = id->tmp_x->next;
 }
+
 void	ft_fill_red(t_command **cmd, t_splitor **x, t_envarment *my_env)
 {
 	t_pre	id;
@@ -211,47 +109,5 @@ void	ft_fill_red(t_command **cmd, t_splitor **x, t_envarment *my_env)
 			id.i++;
 		}
 		ft_next(&id.tmp_x, &id.tmp_cmd);
-	}
-}
-void	ft_fill_store(t_command **tmp_cmd)
-{
-	int			i;
-	t_redirect	*tmp;
-
-	i = 0;
-	(*tmp_cmd)->store_her = NULL;
-	(*tmp_cmd)->len = 0;
-	tmp = (*tmp_cmd)->doc;
-	while (tmp != NULL)
-	{
-		if (tmp->type == HERE_DOC)
-			(*tmp_cmd)->len++;
-		tmp = tmp->next;
-	}
-	(*tmp_cmd)->store_her = malloc(sizeof(char *) * ((*tmp_cmd)->len + 1));
-	tmp = (*tmp_cmd)->doc;
-	while (tmp != NULL)
-	{
-		if (tmp->type == HERE_DOC)
-		{
-			(*tmp_cmd)->store_her[i] = ft_strdup(tmp->store);
-			i++;
-		}
-		tmp = tmp->next;
-	}
-	(*tmp_cmd)->store_her[i] = NULL;
-}
-void	ft_fill_her(t_command **new_node)
-{
-	t_command	*tmp_cmd;
-
-	tmp_cmd = *new_node;
-	while (tmp_cmd != NULL)
-	{
-		ft_fill_store(&tmp_cmd);
-		if (tmp_cmd != NULL)
-			tmp_cmd = tmp_cmd->next;
-		if (tmp_cmd != NULL && tmp_cmd->is_pipe)
-			tmp_cmd = tmp_cmd->next;
 	}
 }
---
+++ skip_redirection.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   skip_redirection.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-mora <rel-mora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 19:53:40 by rel-mora          #+#    #+#             */
+/*   Updated: 2024/09/21 20:29:15 by rel-mora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*ft_fill_final(char **s)
+{
+	int		i;
+	char	*final;
+
+	final = NULL;
+	i = 0;
+	while (s[i] != NULL)
+	{
+		final = ft_strjoin(final, s[i]);
+		final = ft_strjoin(final, " ");
+		i++;
+	}
+	return (final);
+}
+
+int	ft_check_ambiguous(t_splitor *tmp_x, t_envarment *my_env)
+{
+	char	*s;
+	char	**str;
+
+	s = NULL;
+	while ((tmp_x) != NULL && (tmp_x)->state == G && !redirection(tmp_x)
+		&& tmp_x->type != ' ')
+	{
+		if (tmp_x->type == '$' && tmp_x->state == G)
+		{
+			s = ft_expand(tmp_x->in, &my_env);
+			if (s == NULL)
+				return (0);
+			str = ft_split(s, ' ');
+			if (ft_len_arg(str) > 1)
+				return (1);
+		}
+		tmp_x = tmp_x->next;
+	}
+	return (0);
+}
+
+void	ft_check_quote(t_splitor **tmp_x, char **final)
+{
+	if (((*tmp_x) != NULL && (*tmp_x)->next != NULL) && (((*tmp_x)->type == '\"'
+				&& (*tmp_x)->next->type == '\"') || ((*tmp_x)->type == '\''
+				&& (*tmp_x)->next->type == '\'')))
+	{
+		*final = ft_strdup("\0");
+		(*tmp_x) = (*tmp_x)->next;
+		(*tmp_x) = (*tmp_x)->next;
+	}
+}
+
+char	*ft_skip_direction(t_splitor **tmp_x, t_envarment *my_env, int *is_amb,
+		int her)
+{
+	char	*final;
+	char	**str;
+
+	str = malloc(sizeof(char *));
+	str[0] = NULL;
+	final = NULL;
+	ft_check_quote(tmp_x, &final);
+	if ((*tmp_x) != NULL && (*tmp_x)->state == G && ((*tmp_x)->type == '\"'
+			|| (*tmp_x)->type == '\''))
+	{
+		ft_double_and_sigle(tmp_x, my_env, her, &str);
+		final = ft_fill_final(str);
+	}
+	else if ((*tmp_x) != NULL && (*tmp_x)->state == G)
+	{
+		*is_amb = ft_check_ambiguous((*tmp_x), my_env);
+		if (*is_amb == 1)
+			return (NULL);
+		ft_word(tmp_x, my_env, her, &str);
+		final = ft_fill_final(str);
+	}
+	return (final);
+}
